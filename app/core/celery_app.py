b8cfbdc062168e2921a5a8f8771624a24f11fbbf
Aayush-Raut10 <aayushraut209@gmail.com>
2026-06-21 22:15:16 +0545

    Configuring the celery for queue framework

---
+++ app/core/celery_app.py
@@ -0,0 +1,17 @@
+import time
+from celery import Celery
+from app.core.config  import settings
+
+# Connect to Redis broker and result backend
+celery_app = Celery(
+    "worker", 
+    broker = settings.REDIS_URL, 
+    backend = settings.REDIS_URL
+)
+
+celery_app.conf.update(
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"],
+    timezone="UTC",
+)
