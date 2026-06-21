1620544864935fcff3e0ac913a2261fb3ef3935e
Aayush-Raut10 <aayushraut209@gmail.com>
2026-06-21 22:39:57 +0545

    Solving cors issue

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from app.routers import process
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Image processing API")
 
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:8080",
+]
 
-app.include_router()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(process.router)
--- app/routers/process.py
+++ app/routers/process.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 
-router = APIRouter(prefix="/api/v1/process")
+router = APIRouter(prefix="/api/v1/images")
 
 
 @router.post("/")
